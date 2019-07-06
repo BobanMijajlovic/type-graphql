ef07e016c035db21b8a1b562124dff81ff8df0ee
Michał Lytek <michal.ly@wp.pl>
2019-07-06 18:04:04 +0200

    wip: configure lerna and changelog

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   verbose: false,
   collectCoverage: false,
-  // testEnvironment: "node",
-  // transform: {
-  //   "^.+\\.tsx?$": "ts-jest",
-  // },
-  // moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   rootDir: "./",
   projects: ["<rootDir>/packages/*"],
-  // coverageDirectory: "<rootDir>/coverage",
-  // collectCoverageFrom: [
-  //   "<rootDir>/packages/**/src/**/*.ts",
-  //   "!<rootDir>/packages/**/*.d.ts",
-  // ],
 };
--- packages/class-validator/src/index.ts
+++ packages/class-validator/src/index.ts
@@ -1,3 +1,6 @@
 import foobar from "@src/foobar";
+import ensureInstalledCorrectDepencenciesVersions from "@src/check-deps-version";
+
+ensureInstalledCorrectDepencenciesVersions();
 
 export { foobar };
